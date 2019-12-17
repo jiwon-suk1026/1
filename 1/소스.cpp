3686e67c2b7eed78bba62980d57af9998801b115
지원 석 <wb001026@naver.com>
2019-12-17 23:07:43 +0900

    초기 커밋.

---
+++ 1/소스.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int intpow(int a, int b);
+
+int main()
+{
+	int m, n;
+	printf("두 개의 정수를 입력하세요: ");
+	scanf("%d%d", &m, &n);
+	printf("함수의 결과는 %d입니다.\n", intpow(m, n));
+
+}
+
+int intpow(int a, int b)
+{
+	int mn = 0;
+	mn = a * b;
+
+	return mn;
+}
